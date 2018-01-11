a63bdc4263e4dfacf12447b5473ed9b974a63849
Sidharth Mehra <sidharth.mehra@copart.com>
2018-01-11 01:01:31 -0600

    resolve assets load issue for static build

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    template: './docs-styleguidist/template.html',
-    skipComponentsWithoutExample: true,
-    title: 'Ops-portal UI Styleguide',
-    showCode: true,
-    assetsDir: './public'
+  template: './docs-styleguidist/template.html',
+  skipComponentsWithoutExample: true,
+  title: 'G2 Ops UI Styleguide',
+  showCode: false,
+  assetsDir: './docs-styleguidist'
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,64 @@
-const path = require("path")
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const { join, resolve } = path
 const root = resolve(__dirname)
 const src = join(root, 'src')
 const dest = join(root, 'public')
 
-const HtmlWebpackPlugin = require("html-webpack-plugin")
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: "./src/index.html",
-  filename: "index.html",
-  inject: "body",
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
 })
+
 module.exports = {
   devtool: 'source-map',
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
     publicPath: '/',
     path: dest,
-    filename: "bundle.js",
+    filename: 'bundle.js'
   },
   resolve: {
     alias: {
       examples: join(src, 'examples'),
       components: join(src, 'components'),
-      assets: join(dest, 'assets'),
     },
-    extensions: [".jsx", ".js", ".css"],
+    extensions: [ '.jsx', '.js', '.css' ]
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: "babel-loader", exclude: /node_modules/ },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
       {
         test: /\.(css|less)/,
         use: [
-          { loader: "style-loader" },
+          { loader: 'style-loader' },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               importLoaders: true,
               sourceMap: true,
               modules: true,
-              localIdentName: "[name]__[local]___[hash:base64:5]",
-            },
-          },
-        ],
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          }
+        ]
       },
       {
         test: /\.(scss|sass)$/,
         use: [
           {
-            loader: "style-loader", // creates style nodes from JS strings
+            loader: 'style-loader' // creates style nodes from JS strings
           },
           {
-            loader: "css-loader", // translates CSS into CommonJS
+            loader: 'css-loader' // translates CSS into CommonJS
           },
           {
-            loader: "sass-loader", // compiles Sass to CSS
-          },
-        ],
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
+        ]
       },
       {
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
@@ -66,12 +66,12 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 10000,
-            },
-          },
-        ],
-      },
-    ],
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
   },
-  plugins: [HtmlWebpackPluginConfig],
+  plugins: [ HtmlWebpackPluginConfig ]
 }
