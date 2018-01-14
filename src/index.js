0c529d543c9303ae4537e721c9cae7237185d0ba
Sidharth Mehra <sidharth.mehra@copart.com>
2018-01-13 20:08:03 -0600

    Expose components in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,85 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.js'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App.js'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+// ReactDOM.render(<App />, document.getElementById('app'))
+
+export { default as AppBar } from './components/AppBar'
+export { default as Avatar } from './components/Avatar'
+// export { default as BottomNavigation, BottomNavigationAction } from './components/BottomNavigation'
+// export { default as Button } from './components/Button'
+// export { default as ButtonBase } from './components/ButtonBase'
+// export { default as Card, CardActions, CardContent, CardHeader, CardMedia } from './components/Card'
+// export { default as Checkbox } from './components/Checkbox'
+// export { default as Chip } from './components/Chip'
+// export { default as ClickAwayListener } from './components/utils/ClickAwayListener'
+// export {
+//   default as Dialog,
+//   DialogActions,
+//   DialogContent,
+//   DialogContentText,
+//   DialogTitle,
+//   withMobileDialog,
+// } from './components/Dialog'
+// export { default as Divider } from './components/Divider'
+// export { default as Drawer } from './components/Drawer'
+// export {
+//   default as ExpansionPanel,
+//   ExpansionPanelActions,
+//   ExpansionPanelDetails,
+//   ExpansionPanelSummary,
+// } from './components/ExpansionPanel'
+// export { FormControl, FormGroup, FormLabel, FormHelperText, FormControlLabel } from './components/Form'
+// export { default as Hidden } from './components/Hidden'
+// export { default as Icon } from './components/Icon'
+// export { default as IconButton } from './components/IconButton'
+// export { default as Input, InputLabel, InputAdornment } from './components/Input'
+// export { default as Grid } from './components/Grid'
+// export { default as GridList, GridListTile, GridListTileBar } from './components/GridList'
+// export {
+//   default as List,
+//   ListItem,
+//   ListItemAvatar,
+//   ListItemIcon,
+//   ListItemSecondaryAction,
+//   ListItemText,
+//   ListSubheader,
+// } from './components/List'
+// export { default as Menu, MenuItem, MenuList } from './components/Menu'
+// export { default as Modal, Backdrop, ModalManager } from './components/Modal'
+// export { default as Paper } from './components/Paper'
+// export { default as Popover } from './components/Popover'
+// export { default as Portal } from './components/Portal'
+// export { CircularProgress, LinearProgress } from './components/Progress'
+// export { default as Radio, RadioGroup } from './components/Radio'
+// export { default as Reboot } from './components/Reboot'
+// export { default as Select } from './components/Select'
+// export { default as Snackbar, SnackbarContent } from './components/Snackbar'
+// export { default as Stepper, Step, StepButton, StepContent, StepLabel } from './components/Stepper'
+// export { MuiThemeProvider, withStyles, withTheme, createMuiTheme, jssPreset } from './components/styles'
+
+// // eslint-disable-next-line import/first
+// import * as colors from './components/colors'
+
+// export { colors }
+
+// export { default as SvgIcon } from './components/SvgIcon'
+// export { default as Switch } from './components/Switch'
+// export {
+//   default as Table,
+//   TableBody,
+//   TableCell,
+//   TableFooter,
+//   TableHead,
+//   TablePagination,
+//   TableRow,
+//   TableSortLabel,
+// } from './components/Table'
+// export { default as Tabs, Tab } from './components/Tabs'
+// export { default as Typography } from './components/Typography'
+// export { default as TextField } from './components/TextField'
+// export { default as Toolbar } from './components/Toolbar'
+// export { default as Tooltip } from './components/Tooltip'
+// export { Slide, Grow, Fade, Collapse, Zoom } from './components/transitions'
+
+// export { default as withWidth } from './components/utils/withWidth'
