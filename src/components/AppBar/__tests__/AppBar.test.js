e648d6623c1e41fe14f654b71a6fb3402e67187a
ankatta <anilreddykatta@hotmail.com>
2018-02-03 00:51:41 -0600

    Test cases for AppBar

--- src/components/AppBar/__tests__/AppBar.test.js
+++ src/components/AppBar/__tests__/AppBar.test.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { mount } from 'enzyme'
+import renderer from 'react-test-renderer'
+
+import { wrapWithMaterialUIContext } from 'utils/wrapWithContext'
 
 import AppBar from '../AppBar'
 
+const getProps = (extraProps = {}) => ({
+  ...extraProps,
+  config: []
+})
+
 describe('<AppBar />', () => {
-  test('<AppBar /> rendered properly', () => {
-    expect(true).toBeTruthy()
+  test('<AppBar /> renders properly', () => {
+    expect(renderer.create(wrapWithMaterialUIContext(<AppBar {...getProps()} />))).toMatchSnapshot()
+  })
+
+  test('<AppBar /> render LogOut Menu when Logged In', () => {
+    const tree = mount(wrapWithMaterialUIContext(<AppBar {...getProps({ isLoggedOn: true })} />))
+    expect(tree.find('LogoutMenu').exists()).toBeTruthy()
   })
 })
