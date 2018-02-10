892e81fd139dda415ea5682b3883c618d8acfd19
ankatta <anilreddykatta@hotmail.com>
2018-02-10 16:58:55 -0600

    Reverting App.js

--- g2-layout-demo/src/App.js
+++ g2-layout-demo/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
 import Demo from './demo/demo'
 
-const App = () => <Demo />
+const App = () => (
+  <Router>
+    <Route path="/" component={Demo} />
+  </Router>
+)
 
 export default App
