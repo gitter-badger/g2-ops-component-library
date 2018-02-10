0f6ecbf2905c6b0b892a70f641d6333fa607facf
ankatta <anilreddykatta@hotmail.com>
2018-02-10 13:51:56 -0600

    Pushing Demo Page

--- g2-layout-demo/src/App.js
+++ g2-layout-demo/src/App.js
@@ -1,14 +1,6 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
 import Demo from './demo/demo'
 
-const App = () => (
-  <Router>
-    <Route exact path="/" component={Demo} />
-  </Router>
-)
+const App = () => <Demo />
 
 export default App
