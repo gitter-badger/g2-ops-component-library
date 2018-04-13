828e4b91f425f4e3f44ba3822f165ab981506f5d
Colton Colcleasure <cocolcleasure@MM-C03844.local>
2018-04-13 11:05:52 -0500

    Updated eslint config.
    eslint should handle code quality while prettier manages code styling. eslint should not be warning or breaking on styling inconsistencies, as prettier's purpose is specifically to make it so that we don't have to waste any mental energy thinking about that domain.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,43 +3,43 @@ const customImportResolver = path.resolve('./flow.resolver')
 
 module.exports = {
   parser: 'babel-eslint',
+
   env: {
     browser: true,
     node: true,
     mocha: true,
   },
+
   plugins: ['react', 'flowtype'],
   extends: 'eslint-config-airbnb-easy',
+
   settings: {
     'import/resolver': {
       node: {
         extensions: ['.js'],
         paths: ['node_modules', path.join(__dirname, 'src')],
       },
+
       [customImportResolver]: {
         moduleNameMapper: {
           '^types/(.*)': './types/$1',
         },
       },
     },
+
     flowtype: {
       onlyFilesWithFlowAnnotation: false,
     },
   },
+
   rules: {
-    'array-bracket-spacing': [1, 'always'],
-    eqeqeq: [2, 'smart'],
-    'jsx-quotes': [1, 'prefer-double'],
-    'no-console': 0,
     'no-unused-vars': 0,
-    'object-curly-spacing': [1, 'always'],
-    quotes: [1, 'single', 'avoid-escape'],
-    'react/jsx-space-before-closing': [1, 'never'],
+    'no-console': 0,
     'react/no-did-mount-set-state': 0,
     'react/prop-types': 0,
-    semi: [1, 'never'],
-    'space-before-blocks': [1, 'always'],
+    eqeqeq: [2, 'smart'],
   },
+
   globals: {
     ReactComponent: false,
     ReactElement: false,
