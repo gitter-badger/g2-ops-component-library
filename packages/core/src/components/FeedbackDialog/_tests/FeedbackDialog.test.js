8ec7d72533b08a43d66d13b50f1492bea7add6bf
Colton Colcleasure <cocolcleasure@MM-C03844.local>
2018-05-24 02:11:20 -0500

    Added Tests.

--- packages/core/src/components/FeedbackDialog/_tests/FeedbackDialog.test.js
+++ packages/core/src/components/FeedbackDialog/_tests/FeedbackDialog.test.js
@@ -1,50 +1,51 @@
 import * as React from 'react'
 import { shallow, mount } from 'enzyme'
+
 import { FeedbackDialog } from '../FeedbackDialog'
-import { wrapWithMaterialUIContext as mui } from 'utilities/wrapWithContext'
+import { Checkbox } from 'components/Checkbox'
+import { Button } from "components/Button";
+import { TextField } from "components/TextField";
+import { AutoSelect } from "components/AutoSelect";
+import { DialogBox } from "components/DialogBox";
+import { wrapWithMaterialUIContext} from 'utilities/wrapWithContext'
+
+const afterSendFeedback = jest.fn()
+
+const Component = (props) => {
+	return wrapWithMaterialUIContext(
+		<FeedbackDialog
+			afterSendFeedback={afterSendFeedback}
+			userEmail="STUB"
+			selectedYard={999}
+			homeYard={888}
+			selectedRole="STUB"
+			countryCode="US"
+			language="STUB"
+		/>
+	)
+}
+
+const diveToComponent = () => {
+	return shallow(<Component />).find(FeedbackDialog).dive()
+}
 
 it('should shallow render without erring', () => {
-	const wrapper = shallow(
-		mui(<FeedbackDialog
-			userEmail="string"
-			sendFeedback={() => {}}
-			selectedYard={700}
-			homeYard={12}
-			selectedRole="FOO"
-			countryCode="US"
-			language="string"
-		/>)
-	)
-
-	expect(wrapper).toBeDefined()
-})
-
-it('should mount without erring', () => {
-	const wrapper = mount(
-		mui(<FeedbackDialog
-			userEmail="tring"
-			sendFeedback={() => {}}
-			selectedYard={700}
-			homeYard={12}
-			selectedRole="FOO"
-			countryCode="US"
-			language="tring"
-		/>)
-	)
-
+	const wrapper = shallow(<Component />)
 	expect(wrapper).toBeDefined()
 })
 
 it('should react to visibility state change', () => {
-	expect(true).toEqual(true)
-})
-
-it('should be initialized with correct state', () => {
-	// open
-	// enhancement
-	expect(true).toEqual(true)
-})
-
-it('should react to visibility state change', () => {
-	expect(true).toEqual(true)
+	const wrapper = diveToComponent()
+	wrapper.setState({ enhancementChosen: true })
+	expect(wrapper.find(Checkbox).length).toEqual(0)
+	expect(wrapper.find(TextField).length).toEqual(0)
+	expect(wrapper.find(Button).length).toEqual(0)
+	wrapper.setState({ enhancementChosen: false })
+	expect(wrapper.find(Checkbox).length).toEqual(1)
+	expect(wrapper.find(TextField).length).toEqual(1)
+	expect(wrapper.find(Button).length).toEqual(1)
+	wrapper.setState({ enhancementChosen: true })
+	expect(wrapper.find(Checkbox).length).toEqual(0)
+	expect(wrapper.find(TextField).length).toEqual(0)
+	expect(wrapper.find(Button).length).toEqual(0)
 })
